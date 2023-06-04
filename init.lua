1a4c57354e5f1c25e3381915082e7ed3f2c2ad7e
Chris Patti <feoh@feoh.org>
2023-06-04 16:10:59 +0000

    Revert "Refactor theme and status line into their own file"
    This reverts commit cdaa750c86cfc0c8f2fc1c8638b7296c327bf5a9.

--- init.lua
+++ init.lua
@@ -122,18 +122,23 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        vim.keymap.set('n', '[c', require('gitsigns').prev_hunk, { buffer = bufnr, desc = 'Go to Previous Hunk' })
-        vim.keymap.set('n', ']c', require('gitsigns').next_hunk, { buffer = bufnr, desc = 'Go to Next Hunk' })
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
+          { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
         vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
       end,
     },
   },
 
+
   -- Theme related configs go here
   require 'kickstart.plugins.theme',
 
+
+
   -- Status line related configs go here
   require 'kickstart.plugins.statusline',
+
 
   {
     -- Add indentation guides even on blank lines
