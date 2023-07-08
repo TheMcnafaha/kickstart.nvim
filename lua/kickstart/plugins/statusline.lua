amninder/dotfile

Refactor theme and status line into their own file

lua/kickstart/plugins/statusline.lua
@@ -1,0 +1,14 @@
+return  {
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = false,
+      theme = 'tokyonight',
+      component_separators = '|',
+      section_separators = '',
+    },
+  },
+}
+

TheMcnafaha/kickstart.nvim

lua/kickstart/plugins/statusline.lua
@@ -1,0 +1,13 @@
+return {
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = false,
+      theme = 'moonfly',
+      component_separators = '|',
+      section_separators = '',
+    },
+  },
+}