7ab8df1053632a61e7c13a0d880c371344f17b24
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-02-09 23:20:18 -0500

    added missing remaps/configs

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -1,3 +1,4 @@
+-- p stuff
 vim.api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", { desc = "Shift selected area upwards" })
 vim.api.nvim_set_keymap('v', 'K', ":m '>-2<CR>gv=gv", { desc = "Shift selected area downwards" })
 vim.api.nvim_set_keymap('x', '<leader>p', '\"_dP',
@@ -7,5 +8,8 @@ vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, desc = "cente
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, desc = "center screen on up half page" })
 vim.api.nvim_set_keymap('n', 'n', 'nzzzv', { desc = "go foward a  search while keeping cursor centered" })
 vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', { desc = "go bakwards a  search while keeping cursor centered" })
+-- not remap
+vim.opt.scrolloff = 15
+vim.wo.relativenumber = true
 -- my stuff
 vim.keymap.set('n', '<leader>l', '<Cmd>Neotree toggle<CR>', { desc = 'toggle file tree' })
