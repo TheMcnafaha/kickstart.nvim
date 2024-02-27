cb50e760fc9eaf67dd9f6e7914f2d5953cae9586
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-02-26 20:12:27 -0500

    feat: first good snippet

---
+++ lua/custom/qsnippets.lua
@@ -0,0 +1,45 @@
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.expand_conditions")
+-- qwik component snippets (tsx)
+require("luasnip.session.snippet_collection").clear_snippets("lua")
+local strg = vim.api.nvim_buf_get_name(0)
+print(strg, "lolo lolo 3")
+
+local favSn = s("deftype", fmt([[
+<{}>
+]], {
+	i(1)
+}))
+
+ls.add_snippets("lua", {
+	s("qsignal", fmta([[
+const <var> = useSignal<type>(<initial>)
+]], {
+		var = i(1),
+		type = i(2),
+		initial = i(3)
+	}))
+})
+ls.add_snippets("lua", { favSn
+})
+
+
+return strg
