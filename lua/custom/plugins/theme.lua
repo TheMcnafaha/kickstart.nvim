93fb0ff3082b1940cabbd77e6bb7c192ea8f26f7
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-08-30 01:41:30 -0400

    feat(plugins): enables possibly good default plugs

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,10 +1,21 @@
 -- File: lua/custom/plugins/autopairs.lua
+-- X~b:3UrcQ)#AcXHqS2i-
 return {
-	'bluz71/vim-moonfly-colors',
-	priority = 1000,
-	name = "moonfly",
-	lazy = false,
-	config = function()
-		vim.cmd.colorscheme 'moonfly'
-	end
+  'bluz71/vim-moonfly-colors',
+  priority = 1000,
+  name = 'moonfly',
+  lazy = false,
+  config = function()
+    vim.cmd.colorscheme 'moonfly'
+  end,
 }
+-- return {
+--   'bluz71/vim-nightfly-colors',
+--   name = 'nightfly',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     -- Lua initialization file
+--     vim.cmd.colorscheme 'nightfly'
+--   end,
+-- }
